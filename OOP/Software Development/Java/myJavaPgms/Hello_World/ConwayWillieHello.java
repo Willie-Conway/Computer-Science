b51d1fdfe207d78b9e67acbbe91a91dcb8165404
Willie ( J. )  Conway <85038615+Willie-Conway@users.noreply.github.com>
2023-08-29 20:04:56 -0400

    Add files via upload

---
+++ OOP/Software Development/Java/myJavaPgms/Hello_World/ConwayWillieHello.java
@@ -0,0 +1,13 @@
+/*
+ * Willie Conway
+ * ConwayWillieHello.java
+ * 
+ *  Create jave file with LastNameFirstNameHello
+ *  Create program that Printsline "Hello, World!"
+ */
+
+public class ConwayWillieHello {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
